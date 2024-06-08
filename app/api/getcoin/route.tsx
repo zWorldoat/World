a295021e7635fad8bf58ca506075efdc5425d645
TeryNobparat <nobparat.bun@gmail.com>
2024-06-08 15:08:15 +0700

    Update Type

--- app/api/getcoin/route.tsx
+++ app/api/getcoin/route.tsx
@@ -4,6 +4,80 @@ import { COIN_LISTS } from "./coinlist";
 // import sha256 from "crypto-js/sha256";
 // import { enc } from "crypto-js";
 
+interface TickerData {
+  close: string;
+  high: string;
+  last: string;
+  low: string;
+  market: string;
+  open: string;
+  period: number;
+  value: string;
+  volume: string;
+  volume_buy: string;
+  volume_sell: string;
+}
+
+interface AssetConfigData {
+  ccy: string;
+  deposit_enabled: boolean;
+  withdraw_enabled: boolean;
+  inter_transfer_enabled: boolean;
+  is_st: boolean;
+}
+
+interface ChainData {
+  chain: string;
+  min_deposit_amount: string;
+  min_withdraw_amount: string;
+  deposit_enabled: boolean;
+  withdraw_enabled: boolean;
+  deposit_delay_minutes: number;
+  safe_confirmations: number;
+  irreversible_confirmations: number;
+  deflation_rate: string;
+  withdrawal_fee: string;
+  withdrawal_precision: number;
+  memo: string;
+  is_memo_required_for_deposit: boolean;
+  explorer_asset_url: string;
+}
+
+interface DepthData {
+  asks: Array<[string, string]>;
+  bids: Array<[string, string]>;
+  checksum: number;
+  last: string;
+  updated_at: number;
+}
+
+interface PriceData {
+  id: string;
+  mintSymbol: string;
+  vsToken: string;
+  vsTokenSymbol: string;
+  price: number;
+}
+
+interface CoinPriceData {
+  [key: string]: PriceData;
+}
+
+interface CoinData {
+  name: string;
+  ticker: TickerData;
+  assetConfig: AssetConfigData;
+  chains: ChainData;
+  depth: DepthData;
+  price: CoinPriceData;
+  timestamp: number;
+}
+
+interface Coin {
+  name: string;
+  id: string;
+}
+
 // Function to fetch data for a coin
 async function fetchCoinData(coin: any) {
   try {
@@ -22,12 +96,12 @@ async function fetchCoinData(coin: any) {
 
     return {
       name: coin.name,
-      ticker: tickerResponse.data.data[0],
-      assetConfig: assetConfigResponse.data.data.asset,
-      chains: assetConfigResponse.data.data.chains[0],
-      depth: depthResponse.data.data.depth,
-      price: priceResponse.data.data,
-      timestamp: priceResponse.data.timeTaken,
+      ticker: tickerResponse.data.data[0] as TickerData,
+      assetConfig: assetConfigResponse.data.data.asset as AssetConfigData,
+      chains: assetConfigResponse.data.data.chains[0] as ChainData,
+      depth: depthResponse.data.data.depth as DepthData,
+      price: priceResponse.data.data as CoinPriceData,
+      timestamp: priceResponse.data.timeTaken as number,
     };
   } catch (error: any) {
     console.error(`Error fetching data for ${coin.name}:`, error.message);
